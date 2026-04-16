576d78ab1c44b1817f06aeb88a3e2ff57f06b6b4
gsouzar <gugu2712.rodrigues@gmail.com>
2026-04-16 15:11:13 -0300

    Padronização completa de conexão e segurança

--- Backend/python_api/app/core/config.py
+++ Backend/python_api/app/core/config.py
@@ -1,6 +1,12 @@
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+#Esta linha lê o arquivo .env e carrega as variáveis no sistema
+load_dotenv()
+#Endereço do Banco de Dados
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://senaihe:senha_do_db@127.0.0.1:3306/senai_horas_extensao")
+#Chaves de Segurança para o Login 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+#Tempo de expiração do login
 TOKEN_EXPIRATION = int(os.getenv("TOKEN_EXPIRATION"))
